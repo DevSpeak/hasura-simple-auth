40fb63628f5c7f4e779c205014b18ee62e6f683e
Sergey Mount <i@b9sk.ru>
2021-05-28 15:42:21 +0300

    make the example work with hasura v2

--- resolvers.js
+++ resolvers.js
@@ -12,19 +12,19 @@ const graphql = new GraphQLClient(process.env.ENDPOINT, {
 
 const LOGIN = `
   query($email: String) {
-    user(where:{email: {_eq: $email}}) { id password }
+    users(where:{email: {_eq: $email}}) { id password }
   }
 `
 
 const SIGNUP = `
   mutation($email: String, $password: String) {
-    insert_user(objects: { email: $email, password: $password }) { returning { id }}
+    insert_users(objects: { email: $email, password: $password }) { returning { id }}
   }
 `
 
 const ME = `
-  query($id: uuid) {
-    user(where:{id: {_eq: $id}}) { email }
+  query($id: bigint) {
+    users(where:{id: {_eq: $id}}) { email }
   }
 `
 
@@ -33,7 +33,7 @@ const resolvers = {
     me: async (_, args, req) => {
       if (req.userId) {
         const user = await graphql.request(ME, { id: req.userId }).then(data => {
-          return data.user[0]
+          return data.users[0]
         })
         return { ...user }
       } else {
@@ -46,13 +46,13 @@ const resolvers = {
       const hashedPassword = await bcrypt.hash(password, 10)
 
       const user = await graphql.request(SIGNUP, { email, password: hashedPassword }).then(data => {
-        return data.insert_user.returning[0]
+        return data.insert_users.returning[0]
       })
 
       const token = jwt.sign({
         userId: user.id,
         'https://hasura.io/jwt/claims': {
-          'x-hasura-user-id': user.id,
+          'x-hasura-user-id': user.id + "",
           'x-hasura-default-role': 'user',
           'x-hasura-allowed-roles': ['user']
         }
@@ -62,7 +62,7 @@ const resolvers = {
     },
     login: async (_, { email, password }) => {
       const user = await graphql.request(LOGIN, { email }).then(data => {
-        return data.user[0]
+        return data.users[0]
       })
 
       if (!user) throw new Error('No such user found.')
@@ -73,7 +73,7 @@ const resolvers = {
         const token = jwt.sign({
           userId: user.id,
           'https://hasura.io/jwt/claims': {
-            'x-hasura-user-id': user.id,
+            'x-hasura-user-id': user.id + "",
             'x-hasura-default-role': 'user',
             'x-hasura-allowed-roles': ['user']
           }
